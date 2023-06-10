5438adfc18f159db1e38fb3ec4aa8f51cdc8917f
Sursulet <57486825+Sursulet@users.noreply.github.com>
2023-06-10 16:09:46 +0200

    Update MainViewModel.java

--- app/src/main/java/com/cleanup/todoc/ui/MainViewModel.java
+++ app/src/main/java/com/cleanup/todoc/ui/MainViewModel.java
@@ -2,5 +2,20 @@ package com.cleanup.todoc.ui;
 
 import androidx.lifecycle.ViewModel;
 
+import com.cleanup.todoc.model.Task;
+import com.cleanup.todoc.repository.ProjectRepository;
+import com.cleanup.todoc.repository.TaskRepository;
+
 public class MainViewModel extends ViewModel {
+
+    private ProjectRepository mProjectRepository;
+    private TaskRepository mTaskRepository;
+
+    public MainViewModel(ProjectRepository projectRepository, TaskRepository taskRepository) {
+        this.mProjectRepository = projectRepository;
+        this.mTaskRepository = taskRepository;
+    }
+
+    public void insert(Task task) {}
+    public void delete(Task task) {}
 }
