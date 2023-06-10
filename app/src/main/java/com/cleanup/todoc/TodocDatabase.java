4e84bb7cf56738853c2bf502ea460fbad0597702
Sursulet <57486825+Sursulet@users.noreply.github.com>
2023-06-10 04:24:57 +0200

    Update TodocDatabase.java

--- app/src/main/java/com/cleanup/todoc/TodocDatabase.java
+++ app/src/main/java/com/cleanup/todoc/TodocDatabase.java
@@ -1,11 +1,41 @@
 package com.cleanup.todoc;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import com.cleanup.todoc.dao.ProjectDao;
+import com.cleanup.todoc.dao.TaskDao;
 import com.cleanup.todoc.model.Project;
 import com.cleanup.todoc.model.Task;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @Database(entities = {Project.class, Task.class}, version = 1, exportSchema = false)
 public abstract class TodocDatabase extends RoomDatabase {
+    public abstract ProjectDao projectDao();
+
+    public abstract TaskDao taskDao();
+
+    private static volatile TodocDatabase INSTANCE;
+    private static final int NUMBER_OF_THREADS = 4;
+    static final ExecutorService dataWriteExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    static TodocDatabase getInstance(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (TodocDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(
+                            context.getApplicationContext(),
+                            TodocDatabase.class,
+                            "todoc_database"
+                    ).build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 }
