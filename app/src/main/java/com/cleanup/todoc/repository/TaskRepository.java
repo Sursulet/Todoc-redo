89f5255638ea6244e89da096f0e934d3ee62f407
Sursulet <57486825+Sursulet@users.noreply.github.com>
2023-06-10 04:33:56 +0200

    Update TaskRepository.java

--- app/src/main/java/com/cleanup/todoc/repository/TaskRepository.java
+++ app/src/main/java/com/cleanup/todoc/repository/TaskRepository.java
@@ -1,4 +1,16 @@
 package com.cleanup.todoc.repository;
 
+import android.app.Application;
+
+import com.cleanup.todoc.TodocDatabase;
+import com.cleanup.todoc.dao.TaskDao;
+
 public class TaskRepository {
+
+    private TaskDao mDao;
+
+    TaskRepository(Application application) {
+        TodocDatabase db = TodocDatabase.getInstance(application);
+        mDao = db.taskDao();
+    }
 }
