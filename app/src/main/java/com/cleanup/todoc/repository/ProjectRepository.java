a96ebf288379927a6a5d1222525fd167f3a4e63f
Sursulet <57486825+Sursulet@users.noreply.github.com>
2023-06-10 04:33:51 +0200

    Update ProjectRepository.java

--- app/src/main/java/com/cleanup/todoc/repository/ProjectRepository.java
+++ app/src/main/java/com/cleanup/todoc/repository/ProjectRepository.java
@@ -1,4 +1,15 @@
 package com.cleanup.todoc.repository;
 
+import android.app.Application;
+
+import com.cleanup.todoc.TodocDatabase;
+import com.cleanup.todoc.dao.ProjectDao;
+
 public class ProjectRepository {
+    private ProjectDao mDao;
+
+    ProjectRepository(Application application) {
+        TodocDatabase db = TodocDatabase.getInstance(application);
+        mDao = db.projectDao();
+    }
 }
